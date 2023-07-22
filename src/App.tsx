9a16a08e53982e4d201f7f61de5f6090b31f1b05
amanVar06 <amanvarshney212@gmail.com>
2023-07-22 16:26:54 +0530

    initialised local git repo, created a initial basic structure and template of the project for proceding

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Navbar, Footer, Wrapper } from "./sections"
+
+function App() {
+  return (
+    <div className="main-container">
+      <div className="app">
+        <Navbar />
+        <Wrapper />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+export default App
---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+import About from "./About"
+import MyList from "./MyList"
+import Search from "./Search"
+import Pokemon from "./Pokemon"
+import Compare from "./Compare"
+
+export { About, MyList, Search, Pokemon, Compare }
